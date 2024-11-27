fa0d566d42f5a7efb60966b93ccd0de1ad307b2b
Himanshu Kumar <himanshu064@gmail.com>
2024-11-27 12:31:20 +0530

    Initialised the database method

--- src/api/db/connect.ts
+++ src/api/db/connect.ts
@@ -1,3 +1,22 @@
-export const connectDB = () => {
-  return "ok";
-};
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+
+import ENV from "@/env";
+
+class SupabaseDB {
+  private static instance: SupabaseClient | null = null;
+
+  private constructor() {
+    // Private constructor to prevent instantiation
+  }
+
+  public static getInstance(): SupabaseClient {
+    if (!SupabaseDB.instance) {
+      SupabaseDB.instance = createClient(ENV.SUPABASE_URL, ENV.SUPABASE_KEY);
+    }
+    return SupabaseDB.instance;
+  }
+}
+
+const db = SupabaseDB.getInstance();
+
+export default db;
